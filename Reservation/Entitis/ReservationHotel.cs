a3e17d782b3cceff4b4ffcecbebf9008402c5eaa
dkderyk <derykkauafc18@gmail.com>
2024-12-03 05:38:00 -0300

    first commit

---
+++ Reservation/Entitis/ReservationHotel.cs
@@ -0,0 +1,50 @@
+﻿using Reservation.Exceptions;
+
+namespace Reservation.Entitis
+{
+    public class ReservationHotel
+    {
+        public int RoomNumber { get; set; }
+        public DateTime Checkin { get; set; }
+        public DateTime Checkout { get; set; }
+
+        public ReservationHotel(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            if (checkout < checkin)
+            {
+                throw new DomainException("Erro: CheckOut menor que CheckIn");
+            }
+
+            RoomNumber = roomNumber;
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return $"Reserva: \n   Numero: {RoomNumber} \n   Checkin: {Checkin.ToString("dd/MM/yyyy")} \n   Checkout: {Checkout.ToString("dd/MM/yyyy")} ";
+        }
+
+        public int Duration()
+        {
+            TimeSpan result = Checkout - Checkin;
+            int days = result.Days;
+            return days;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            if (checkin < DateTime.Now || checkout < DateTime.Now)
+            {
+                throw new DomainException("Error: Data inserida menor que data de agora");
+            }
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Error: Checkout menor que Checkin");
+            }
+
+            Checkin = checkin;
+            Checkout = checkout;
+        }
+    }
+}
---
+++ Reservation/Program.cs
@@ -0,0 +1,52 @@
+﻿using Reservation.Entitis;
+using Reservation.Exceptions;
+using System;
+
+namespace Reservation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Numero do Quarto: ");
+                int number = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Checkin Date (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Checkout Date (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                ReservationHotel reserva = new ReservationHotel(number, checkin, checkout);
+
+                Console.WriteLine($"{reserva}, {reserva.Duration()} days");
+
+                Console.WriteLine("insira novos dados da Reserva...");
+                Console.WriteLine("Checkin Date (dd/MM/yyyy): ");
+                checkin = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Checkout Date (dd/MM/yyyy): ");
+                checkout = DateTime.Parse(Console.ReadLine());
+
+                reserva.UpdateDates(checkin, checkout);
+
+                Console.WriteLine($"{reserva}, {reserva.Duration()} days");
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+
+
+            Console.WriteLine("END...");
+
+
+
+        }
+    }
+}
